e4542f5ef13470f7d0f09d3ce306df738fa138c4
cnnrbrn <connorbobrien@gmail.com>
2020-03-05 20:42:01 +0200

    Updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React from "react";
-import logo from "./logo.svg";
+import Heading from "./components/layout/Heading";
+import Paragraph from "./components/layout/Paragraph";
+import Layout from "./components/layout/Layout";
 import "./sass/style.scss";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
+		<Layout>
+			<Heading title="Title from prop" subtitle="Subtitle from prop" />
+
+			<Paragraph>This will be the children prop inside the component.</Paragraph>
+		</Layout>
 	);
 }
 
---
+++ src/components/layout/Heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Heading(props) {
+	return (
+		<>
+			<h1>{props.title}</h1>
+			<h2>{props.subtitle}</h2>
+		</>
+	);
+}
+
+export default Heading;
