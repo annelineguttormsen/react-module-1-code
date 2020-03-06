56cf23a10b8a825f2d9f4cd3c19663949fc5b3d0
cnnrbrn <connorbobrien@gmail.com>
2020-03-06 13:55:19 +0200

    Add lesson 2 answers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Heading from "./components/layout/Heading";
 import Paragraph from "./components/layout/Paragraph";
 import Layout from "./components/layout/Layout";
 import Double from "./components/layout/Double";
+import Subheading from "./components/layout/Subheading";
+import Button from "./components/layout/Button";
+import Division from "./components/layout/Division";
 import "./sass/style.scss";
 
 function App() {
@@ -13,6 +16,12 @@ function App() {
 			<Double number={3} multiply={true} />
 
 			<Paragraph>This will be the children prop inside the component.</Paragraph>
+		
+			<Subheading heading="This is the heading" />
+
+			<Button>Click This Button</Button>
+
+			<Division firstNumber={12} secondNumber={4} />
 		</Layout>
 	);
 }
