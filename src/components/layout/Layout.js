b0b852695a6f5e040975a70931bcb5b4a10c929d
cnnrbrn <connorbobrien@gmail.com>
2020-03-07 15:39:06 +0200

    Update Layout.js

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -15,33 +15,33 @@ import Contact from "../contact/Contact";
 function Layout() {
     return (
         <Router>
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <NavLink to="/" exact>
-                <Navbar.Brand>Rick and Morty</Navbar.Brand>
-            </NavLink>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <NavLink to="/" exact className="nav-link">
-                        Home
-                    </NavLink>
-                    <NavLink to="/about" className="nav-link">
-                        About
-                    </NavLink>
-                    <NavLink to="/contact" className="nav-link">
-                        Contact
-                    </NavLink>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-        <Container>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/contact" component={Contact} />
-            </Switch>
-        </Container>
-    </Router>
+			<Navbar bg="dark" variant="dark" expand="lg">
+				<NavLink to="/" exact>
+					<Navbar.Brand>Rick and Morty</Navbar.Brand>
+				</NavLink>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<NavLink to="/" exact className="nav-link">
+							Home
+						</NavLink>
+						<NavLink to="/about" className="nav-link">
+							About
+						</NavLink>
+						<NavLink to="/contact" className="nav-link">
+							Contact
+						</NavLink>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			<Container>
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/about" component={About} />
+					<Route path="/contact" component={Contact} />
+				</Switch>
+			</Container>
+		</Router>
     );
 }
 
