f649e680dbb2b0600d3a7bfa16a747c2925cd5c2
cnnrbrn <connorbobrien@gmail.com>
2020-03-15 12:36:06 +0200

    Updates

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import Heading from "../layout/Heading";
-import CharacterList from "../characters/list/CharacterList";
 
 export function Home() {
     return (
-        <>
         <Heading title="Home" />
-        <CharacterList />
-        </>
     );
 }
 
