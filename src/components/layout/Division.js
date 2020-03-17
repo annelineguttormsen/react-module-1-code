5c1b2247d50ca3390f74605b290f1d697a48e9d8
Anneline <anneline.guttormsen@hotmail.no>
2020-03-17 15:14:22 +0100

    Finished

--- src/components/layout/Division.js
+++ src/components/layout/Division.js
@@ -1,10 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Division(props) {
 
-    const answer = props.firstNumber / props.secondNumber
+    const answer = props.firstNumber / props.secondNumber;
 
 	return <div>{answer}</div>;
+}
+
+Division.propTypes = {
+    answer : PropTypes.number.isRequired,
+    children : PropTypes.node.isRequired
 }
 
 export default Division;
--- src/components/layout/Subheading.js
+++ src/components/layout/Subheading.js
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function Subheading({ heading }) {
 	return (
 		<h3>{heading}</h3>
 	);
+}
+
+Subheading.propTypes = {
+	heading : PropTypes.string.isRequired
 }
 
 export default Subheading;
